5500356bfc9075cc68d10c1fa494cbfee3254a79
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-02 15:37:34 +0700

    Create variable info and next

--- StackUsinglinkedList/Program.cs
+++ StackUsinglinkedList/Program.cs
@@ -4,6 +4,12 @@ namespace StackUsingLinkedList_CSharp
 {
     class Node
     {
-        
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
     }
 }
