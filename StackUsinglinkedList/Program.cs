9ffe11eb66feb0bbbbaf1b43c97504f1ebe5ab16
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-02 16:00:37 +0700

    Create method display

--- StackUsinglinkedList/Program.cs
+++ StackUsinglinkedList/Program.cs
@@ -42,5 +42,9 @@ namespace StackUsingLinkedList_CSharp
             Console.WriteLine("\nThe popped element is: " + top.info);
             top = top.next; //Make top point to the next node in squence
         }
+        public void display()
+        {
+            
+        }
     }
 }
